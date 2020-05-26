2ffc274c50abc9d219d41e69d97608b47c8d8cda
Pietro <pietro.santoro2@gmail.com>
2020-05-26 22:48:21 +0100

    test frontend

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { UserListComponent } from './user-list/user-list.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'users', component: UserListComponent },
   { path: 'adduser', component: AddUserComponent },
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserService } from './service/user-service.service';
 import { AddUserComponent } from './add-user/add-user.component';
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
     AddUserComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
